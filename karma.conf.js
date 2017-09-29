44bc251a42aadcc0d6c30233b86468fb14e7e937
ywong <york.huang@honeywell.com>
2017-09-29 14:22:14 +1000

    configured ci

--- karma.conf.js
+++ karma.conf.js
@@ -1,8 +1,19 @@
 // Karma configuration
 // Generated on Mon Sep 25 2017 17:43:21 GMT+1000 (AUS Eastern Standard Time)
+var travisConfig = {
+    customLaunchers: {
+        Chrome_travis_ci: {
+            base: 'Chrome',
+            flags: ['--no-sandbox']
+        }
+    },
+    singleRun: true,
+    browsers: 'Chrome_travis_ci'
+};
+ 
+var additionalConfig = process.env.TRAVIS ? travisConfig : {}
 
-module.exports = function(config) {
-  config.set({
+var defaultConfig = {
 
     // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '',
@@ -15,9 +26,9 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'test/*.js',
-      'src/*.js',
-      'src/*.css'
+        'test/*.js',
+        'src/*.js',
+        'src/*.css'
     ],
 
 
@@ -45,19 +56,13 @@ module.exports = function(config) {
     // enable / disable colors in the output (reporters and logs)
     colors: true,
 
-
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
-
-
     // enable / disable watching file and executing tests whenever any file changes
     autoWatch: true,
 
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome', 'PhantomJS'],
+    browsers: ['ChromeHeadless'],
 
 
     // Continuous Integration mode
@@ -67,5 +72,8 @@ module.exports = function(config) {
     // Concurrency level
     // how many browser should be started simultaneous
     concurrency: Infinity
-  })
-}
+};
+
+module.exports = function (config) {
+    config.set(Object.assign({ logLevel: config.LOG_INFO }, additionalConfig, defaultConfig));
+};
