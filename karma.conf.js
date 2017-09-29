449acdb4a3f8d79c359853650744d8e41cf31ef8
ywong <york.huang@honeywell.com>
2017-09-29 14:28:18 +1000

    log env

--- karma.conf.js
+++ karma.conf.js
@@ -10,7 +10,9 @@ var travisConfig = {
     singleRun: true,
     browsers: 'Chrome_travis_ci'
 };
- 
+
+console.info(process.env);
+
 var additionalConfig = process.env.TRAVIS ? travisConfig : {}
 
 var defaultConfig = {
@@ -67,7 +69,7 @@ var defaultConfig = {
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false,
+    singleRun: true,
 
     // Concurrency level
     // how many browser should be started simultaneous
