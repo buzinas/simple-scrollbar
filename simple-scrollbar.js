d9f3e4f56e173e9e51943b348e46f964f57bfd95
runerune-sgt <m.maslak@sgtsa.pl>
2019-06-06 15:50:38 +0200

    revert code style hook

--- simple-scrollbar.js
+++ simple-scrollbar.js
@@ -1,138 +1,138 @@
 ;(function(root, factory) {
-	if (typeof exports === 'object') {
-		module.exports = factory(window, document)
-	} else {
-		root.SimpleScrollbar = factory(window, document)
-	}
+  if (typeof exports === 'object') {
+    module.exports = factory(window, document)
+  } else {
+    root.SimpleScrollbar = factory(window, document)
+  }
 })(this, function(w, d) {
-	var raf = w.requestAnimationFrame || w.setImmediate || function(c) { return setTimeout(c, 0); };
+  var raf = w.requestAnimationFrame || w.setImmediate || function(c) { return setTimeout(c, 0); };
 
-	function initEl(el) {
-		if (Object.prototype.hasOwnProperty.call(el, 'data-simple-scrollbar')) return;
-		Object.defineProperty(el, 'data-simple-scrollbar', { value: new SimpleScrollbar(el) });
-	}
+  function initEl(el) {
+    if (Object.prototype.hasOwnProperty.call(el, 'data-simple-scrollbar')) return;
+    Object.defineProperty(el, 'data-simple-scrollbar', { value: new SimpleScrollbar(el) });
+  }
 
-	// Mouse drag handler
-	function dragDealer(el, context) {
-		var lastPageY;
+  // Mouse drag handler
+  function dragDealer(el, context) {
+    var lastPageY;
 
-		el.addEventListener('mousedown', function(e) {
-			lastPageY = e.pageY;
-			el.classList.add('ss-grabbed');
-			d.body.classList.add('ss-grabbed');
+    el.addEventListener('mousedown', function(e) {
+      lastPageY = e.pageY;
+      el.classList.add('ss-grabbed');
+      d.body.classList.add('ss-grabbed');
 
-			d.addEventListener('mousemove', drag);
-			d.addEventListener('mouseup', stop);
+      d.addEventListener('mousemove', drag);
+      d.addEventListener('mouseup', stop);
 
-			return false;
-		});
+      return false;
+    });
 
-		function drag(e) {
-			var delta = e.pageY - lastPageY;
-			lastPageY = e.pageY;
+    function drag(e) {
+      var delta = e.pageY - lastPageY;
+      lastPageY = e.pageY;
 
-			raf(function() {
-				context.el.scrollTop += delta / context.scrollRatio;
-			});
-		}
+      raf(function() {
+        context.el.scrollTop += delta / context.scrollRatio;
+      });
+    }
 
-		function stop() {
-			el.classList.remove('ss-grabbed');
-			d.body.classList.remove('ss-grabbed');
-			d.removeEventListener('mousemove', drag);
-			d.removeEventListener('mouseup', stop);
-		}
-	}
+    function stop() {
+      el.classList.remove('ss-grabbed');
+      d.body.classList.remove('ss-grabbed');
+      d.removeEventListener('mousemove', drag);
+      d.removeEventListener('mouseup', stop);
+    }
+  }
 
-	// Constructor
-	function ss(el) {
-		this.target = el;
+  // Constructor
+  function ss(el) {
+    this.target = el;
 
-		this.direction = w.getComputedStyle(this.target).direction;
+    this.direction = w.getComputedStyle(this.target).direction;
 
-		this.bar = '<div class="ss-scroll">';
+    this.bar = '<div class="ss-scroll">';
 
-		this.wrapper = d.createElement('div');
-		this.wrapper.setAttribute('class', 'ss-wrapper');
+    this.wrapper = d.createElement('div');
+    this.wrapper.setAttribute('class', 'ss-wrapper');
 
-		this.el = d.createElement('div');
-		this.el.setAttribute('class', 'ss-content');
+    this.el = d.createElement('div');
+    this.el.setAttribute('class', 'ss-content');
 
-		if (this.direction === 'rtl') {
-			this.el.classList.add('rtl');
-		}
+    if (this.direction === 'rtl') {
+      this.el.classList.add('rtl');
+    }
 
-		this.wrapper.appendChild(this.el);
+    this.wrapper.appendChild(this.el);
 
-		while (this.target.firstChild) {
-			this.el.appendChild(this.target.firstChild);
-		}
-		this.target.appendChild(this.wrapper);
+    while (this.target.firstChild) {
+      this.el.appendChild(this.target.firstChild);
+    }
+    this.target.appendChild(this.wrapper);
 
-		this.target.insertAdjacentHTML('beforeend', this.bar);
-		this.bar = this.target.lastChild;
+    this.target.insertAdjacentHTML('beforeend', this.bar);
+    this.bar = this.target.lastChild;
 
-		dragDealer(this.bar, this);
-		this.moveBar();
+    dragDealer(this.bar, this);
+    this.moveBar();
 
-		w.addEventListener('resize', this.moveBar.bind(this));
-		this.el.addEventListener('scroll', this.moveBar.bind(this));
-		this.el.addEventListener('mouseenter', this.moveBar.bind(this));
+    w.addEventListener('resize', this.moveBar.bind(this));
+    this.el.addEventListener('scroll', this.moveBar.bind(this));
+    this.el.addEventListener('mouseenter', this.moveBar.bind(this));
 
-		this.target.classList.add('ss-container');
+    this.target.classList.add('ss-container');
 
-		var css = w.getComputedStyle(el);
-		if (css['height'] === '0px' && css['max-height'] !== '0px') {
-			el.style.height = css['max-height'];
-		}
-	
-		this.observer = new MutationObserver(function(list) {
-			if(list.length) this.moveBar();
-		}.bind(this));
-		
-		this.observer.observe(this.el, {
-			childList: true,
-			subtree: true,
-		});
-	}
+    var css = w.getComputedStyle(el);
+    if (css['height'] === '0px' && css['max-height'] !== '0px') {
+      el.style.height = css['max-height'];
+    }
+  
+    this.observer = new MutationObserver(function(list) {
+      if(list.length) this.moveBar();
+    }.bind(this));
+    
+    this.observer.observe(this.el, {
+      childList: true,
+      subtree: true,
+    });
+  }
 
-	ss.prototype = {
-		moveBar: function(e) {
-			var totalHeight = this.el.scrollHeight,
-					ownHeight = this.el.clientHeight,
-					_this = this;
+  ss.prototype = {
+    moveBar: function(e) {
+      var totalHeight = this.el.scrollHeight,
+          ownHeight = this.el.clientHeight,
+          _this = this;
 
-			this.scrollRatio = ownHeight / totalHeight;
+      this.scrollRatio = ownHeight / totalHeight;
 
-			var isRtl = _this.direction === 'rtl';
-			var right = isRtl ?
-				(_this.target.clientWidth - _this.bar.clientWidth + 18) :
-				(_this.target.clientWidth - _this.bar.clientWidth) * -1;
+      var isRtl = _this.direction === 'rtl';
+      var right = isRtl ?
+        (_this.target.clientWidth - _this.bar.clientWidth + 18) :
+        (_this.target.clientWidth - _this.bar.clientWidth) * -1;
 
-			raf(function() {
-				// Hide scrollbar if no scrolling is possible
-				if(_this.scrollRatio >= 1) {
-					_this.bar.classList.add('ss-hidden')
-				} else {
-					_this.bar.classList.remove('ss-hidden')
-					_this.bar.style.cssText = 'height:' + Math.max(_this.scrollRatio * 100, 10) + '%; top:' + (_this.el.scrollTop / totalHeight ) * 100 + '%;right:' + right + 'px;';
-				}
-			});
-		}
-	}
+      raf(function() {
+        // Hide scrollbar if no scrolling is possible
+        if(_this.scrollRatio >= 1) {
+          _this.bar.classList.add('ss-hidden')
+        } else {
+          _this.bar.classList.remove('ss-hidden')
+          _this.bar.style.cssText = 'height:' + Math.max(_this.scrollRatio * 100, 10) + '%; top:' + (_this.el.scrollTop / totalHeight ) * 100 + '%;right:' + right + 'px;';
+        }
+      });
+    }
+  }
 
-	function initAll() {
-		var nodes = d.querySelectorAll('*[ss-container]');
+  function initAll() {
+    var nodes = d.querySelectorAll('*[ss-container]');
 
-		for (var i = 0; i < nodes.length; i++) {
-			initEl(nodes[i]);
-		}
-	}
+    for (var i = 0; i < nodes.length; i++) {
+      initEl(nodes[i]);
+    }
+  }
 
-	d.addEventListener('DOMContentLoaded', initAll);
-	ss.initEl = initEl;
-	ss.initAll = initAll;
+  d.addEventListener('DOMContentLoaded', initAll);
+  ss.initEl = initEl;
+  ss.initAll = initAll;
 
-	var SimpleScrollbar = ss;
-	return SimpleScrollbar;
+  var SimpleScrollbar = ss;
+  return SimpleScrollbar;
 });
